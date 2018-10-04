cbc32eebc2be03fbbafee4a5ebabce5441575349
Lan Huong Nguyen <nlhuong90@gmail.com>
2018-10-04 16:29:06 -0700

    change vignette to download data from repos

--- data-raw/cop1.R
+++ data-raw/cop1.R
@@ -1,23 +1,24 @@
-raw_data_dir <- "~/Downloads/Cop1_data"
-raw_data_file <- file.path(raw_data_dir, "GSE114762.tar")
-data_url <- paste0("http://www.ncbi.nlm.nih.gov/geo/download/", 
-                   "?acc=GSE114762&format=file&token=wfonuieepfwvjit")
+library(tidyverse)
 
-dir.create(path = raw_data_dir)
-download.file(url = data_url, destfile = raw_data_file)
-untar(tarfile=raw_data_file, exdir=raw_data_dir)
+url <- paste0("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE114762&",
+              "format=file&file=GSE114762_raw_counts.csv.gz")
+url2 <- paste0("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE114762&",
+               "format=file&file=GSE114762_gene_data.csv.gz")
+url3 <- paste0("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE114762&",
+               "format=file&file=GSE114762_sample_info.csv.gz")
 
-smp_url <- paste0("http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=",
-                  "GSE114762&targ=gsm&form=text&view=brief&token=wfonuieepfwvjit")
-smp_data_file <- file.path(raw_data_dir, "GSE114762_sample_data.txt")
-download.file(url=smp_url, destfile=smp_data_file)
+raw.data <- read_csv(url) %>% 
+  column_to_rownames("X1") %>%
+  as.matrix()
+feature.data <- read_csv(url2) %>% 
+  as.data.frame() %>%
+  column_to_rownames("X1")
+sample.data <- read_csv(url3) %>% 
+  as.data.frame() %>%
+  column_to_rownames("X1")
 
-sampleDat <- getGEO(filename=smp_data_file)
-# Just select the sample titles as these contain the meta data we need
-sampleDat <- Meta(sampleDat)
+cop1_eset <- ExpressionSet(
+  as.matrix(raw.data), 
+  phenoData = AnnotatedDataFrame(sample.data),
+  featureData = AnnotatedDataFrame(feature.data))
 
-#ID's
-gsm <- sampleDat$geo_accession
-
-# Get the sample meta data
-meta <- sampleDat$characteristics_ch1
